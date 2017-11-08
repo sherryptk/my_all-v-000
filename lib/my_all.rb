1fde595d53c3017978d7dbe72a715c30b100cac9
Sherry <sherryptk@gmail.com>
2017-11-08 01:00:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,9 @@ def my_all?(collection)
    block_return_values << yield(collection[i])
    i += 1
  end
- block_return_values
-end
+ if block_return_values.include?(false)
+     false
+   else
+     true
+   end
+ end
